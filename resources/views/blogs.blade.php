0150da437010df474710f22cea138d21adb99fdc
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-17 14:25:12 +0630

    initialStateOfBlogs

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blogs</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/blog">First Blog</a></h1>
+        <p>Est culpa ipsum aute voluptate aliqua nulla labore sunt dolore id labore. Duis mollit proident aliquip nostrud culpa sint aliqua consequat ex. Magna adipisicing excepteur aute deserunt aliqua mollit duis veniam dolore. Ipsum amet aute nisi id quis. Velit exercitation pariatur culpa ut veniam. Pariatur ut sunt cupidatat sint qui ut aute.</p>
+    </article>
+    <article>
+        <h1><a href="/blog">Second Blog</a></h1>
+        <p>Est culpa ipsum aute voluptate aliqua nulla labore sunt dolore id labore. Duis mollit proident aliquip nostrud culpa sint aliqua consequat ex. Magna adipisicing excepteur aute deserunt aliqua mollit duis veniam dolore. Ipsum amet aute nisi id quis. Velit exercitation pariatur culpa ut veniam. Pariatur ut sunt cupidatat sint qui ut aute.</p>
+    </article>
+    <article>
+        <h1><a href="/blog">Third Blog</a></h1>
+        <p>Est culpa ipsum aute voluptate aliqua nulla labore sunt dolore id labore. Duis mollit proident aliquip nostrud culpa sint aliqua consequat ex. Magna adipisicing excepteur aute deserunt aliqua mollit duis veniam dolore. Ipsum amet aute nisi id quis. Velit exercitation pariatur culpa ut veniam. Pariatur ut sunt cupidatat sint qui ut aute.</p>
+    </article>
+
+</body>
+</html>
