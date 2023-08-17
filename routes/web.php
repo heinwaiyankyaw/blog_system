d0d5102e602bfff09150af47462521f0d57a11f7
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-17 15:12:49 +0630

    3CoolFunctions

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function PHPUnit\Framework\fileExists;
+
 Route::get('/', function () {
     return view('blogs');
 });
 
 Route::get('/blogs/{blog}', function ($fileName) {
     $path = __DIR__ . "/../resources/blogs/$fileName.html";
+    if (!file_exists($path)) {
+        return redirect('/'); //dd,abort,redirect
+    }
     $blog = file_get_contents($path);
     return view('blog', [
         'blog' => $blog,
