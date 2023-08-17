e3533e8dc8e9a30cb8c0371cdd895bf0b619335f
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-17 15:47:46 +0630

    CompleteWildCardContraintAndCache

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,16 @@ Route::get('/', function () {
     return view('blogs');
 });
 
-Route::get('/blogs/{blog}', function ($fileName) {
-    $path = __DIR__ . "/../resources/blogs/$fileName.html";
+Route::get('/blogs/{blog}', function ($slug) {
+    $path = __DIR__ . "/../resources/blogs/$slug.html";
     if (!file_exists($path)) {
         return redirect('/'); //dd,abort,redirect
     }
-    $blog = file_get_contents($path);
+    $blog = cache()->remember("posts.$slug", now()->addMinute(), function () use ($path) {
+        var_dump("File get Content");
+        return file_get_contents($path);
+    });
     return view('blog', [
         'blog' => $blog,
     ]);
-});
+})->where('blog', '[A-z\-\d]+');//whildcardCon
