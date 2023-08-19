59d0e06596a5944f785d79098ce920dfd60bde92
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-19 11:25:54 +0630

    bladeLayoutSystem

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blogs</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    <article>
-        <h1>{{ $blog->title; }}</h1>
-        <small>Published Date :{{ $blog->date; }} </small>
-        <p>{{ $blog->body; }}</p>
-    </article>
-    <a href="/">Go Back</a>
-</body>
-</html>
+@extends('layout')
+@section('title')
+{{ $blog->title }}
+@endsection
+@section('content')
+<div>
+    <h1>{{ $blog->title; }}</h1>
+    <small>Published Date :{{ $blog->date; }} </small>
+    <p>{{ $blog->body; }}</p>
+</div>
+<a href="/">Go Back</a>
+@endsection
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blogs</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-    @foreach ($blogs as $blog)
+@extends('layout')
+@section('title')
+All Blog
+@endsection
+@section('content')
+@foreach ($blogs as $blog)
+<div>
     <h1><a href="/blogs/{{ $blog->slug }}">{{ $blog->title }}</a></h1>
     <small>Published Date : {{ $blog->date }}</small>
     <p>{{ $blog->intro }}</p>
-    @endforeach
-</body>
-</html>
+
+</div>
+@endforeach
+@endsection
