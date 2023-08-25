6eba7bb5e7892b3fea465e5bbd507953fea23e08
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-25 22:09:41 +0630

    CompleteDBseedingAndFaker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Blog::truncate();
+        User::factory()->create();
+
+        $frontend = Category::create([
+            'name' => 'frontend',
+            'slug' => 'frontend'
+        ]);
+
+        $backend = Category::create([
+            'name' => 'backend',
+            'slug' => 'backend'
+        ]);
+
+        Blog::create([
+            'title' => 'HTML AND CSS',
+            'slug' => 'html-and-css',
+            'intro' => 'This is about HTML AND CSS',
+            'body' => 'Culpa dolor qui nostrud veniam labore excepteur nisi nisi pariatur cupidatat proident occaecat. In aliqua anim proident tempor eiusmod sit nisi pariatur cillum do excepteur magna. Lorem irure commodo et pariatur commodo nulla. Laborum fugiat ex qui deserunt sint et cupidatat.',
+            'category_id' => $frontend->id
+        ]);
+
+        Blog::create([
+            'title' => "What is Pure PHP?",
+            'slug' => 'pure-php',
+            'intro' => "This is about pure PHP.",
+            'body' => 'Amet duis excepteur nisi magna eiusmod sit. Sit ad ullamco irure sunt Lorem sunt esse sint qui ad aute amet. Ex tempor officia qui id. Proident sunt occaecat nulla ea commodo nulla labore.',
+            'category_id' => $backend->id,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,12 @@
 
 use App\Models\Blog;
 use App\Models\Category;
-use Illuminate\Support\Facades\DB;
-
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-use function PHPUnit\Framework\fileExists;
 
 Route::get('/', function () {
-    // DB::listen(function ($query) {
-    //     Log::info($query->sql);
-    // }); //logging watch query from database
+
     return view('blogs', [
-        'blogs' => Blog::with('category')->get() //call in eager load or lazy load
+        'blogs' => Blog::with('category')->get()
     ]);
 });
 
@@ -21,10 +15,9 @@ Route::get('/blogs/{blog:slug}', function (Blog $blog) {
     return view('blog', [
         'blog' => $blog,
     ]);
-})->where('blog', '[A-z\-\d]+'); //whildcardCon
+})->where('blog', '[A-z\-\d]+');
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    // dd($category->blogs);
     return view('blogs', [
         'blogs' => $category->blogs
     ]);
