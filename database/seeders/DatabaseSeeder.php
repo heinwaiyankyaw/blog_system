fc87b58fa9cfe296bdc8001f19a5382e7c97f2c0
heinwaiyankyaw <heinwaiyankyaw2001@gmail.com>
2023-08-25 22:21:49 +0630

    CompleteFactoryFaker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,32 +19,12 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Category::truncate();
         Blog::truncate();
+
+        $frontend = Category::factory()->create(['name' => 'frontend']);
+        $backend = Category::factory()->create(['name' => 'backend']);
+
         User::factory()->create();
-
-        $frontend = Category::create([
-            'name' => 'frontend',
-            'slug' => 'frontend'
-        ]);
-
-        $backend = Category::create([
-            'name' => 'backend',
-            'slug' => 'backend'
-        ]);
-
-        Blog::create([
-            'title' => 'HTML AND CSS',
-            'slug' => 'html-and-css',
-            'intro' => 'This is about HTML AND CSS',
-            'body' => 'Culpa dolor qui nostrud veniam labore excepteur nisi nisi pariatur cupidatat proident occaecat. In aliqua anim proident tempor eiusmod sit nisi pariatur cillum do excepteur magna. Lorem irure commodo et pariatur commodo nulla. Laborum fugiat ex qui deserunt sint et cupidatat.',
-            'category_id' => $frontend->id
-        ]);
-
-        Blog::create([
-            'title' => "What is Pure PHP?",
-            'slug' => 'pure-php',
-            'intro' => "This is about pure PHP.",
-            'body' => 'Amet duis excepteur nisi magna eiusmod sit. Sit ad ullamco irure sunt Lorem sunt esse sint qui ad aute amet. Ex tempor officia qui id. Proident sunt occaecat nulla ea commodo nulla labore.',
-            'category_id' => $backend->id,
-        ]);
+        Blog::factory(2)->create(['category_id' => $frontend->id]);
+        Blog::factory(2)->create(['category_id' => $backend->id]);
     }
 }
